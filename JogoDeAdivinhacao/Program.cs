f3897cb447f9bcdc2378cbef87c3645a2a2f1e5a
Marlon Quilante <marlonquilante@gmail.com>
2025-03-20 20:24:57 -0300

    Implementada versão 2 com geração de números aleatórios

--- JogoDeAdivinhacao/Program.cs
+++ JogoDeAdivinhacao/Program.cs
@@ -8,10 +8,15 @@
             Console.WriteLine("Jogo de Adivinhação");
             Console.WriteLine("---------------------------------");
 
+            Random geradorDeNumeros = new Random();
+
+            int numeroSecreto = geradorDeNumeros.Next(1, 21);
+
             Console.Write("Digite um número para chutar: ");
             int numeroDigitado = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("\nVocê digitou o número: " + numeroDigitado);
+            Console.WriteLine("\nO número secreto é: " + numeroSecreto);
 
             Console.ReadLine();
         }
