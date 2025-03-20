cd6f630e8d5d3aa5d3d1b4de82fad65d97679f15
Marlon Quilante <marlonquilante@gmail.com>
2025-03-20 20:02:34 -0300

    Add project files.

---
+++ JogoDeAdivinhacao/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JogoDeAdivinhacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
