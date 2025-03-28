49a61a9b75e2429ad7dcb2c0a97dc3b5c79f2a54
Marlon Quilante <marlonquilante@gmail.com>
2025-03-28 00:04:52 -0300

    Feita uma refatoração no código para ficar de forma estruturada

--- JogoDeAdivinhacao/Program.cs
+++ JogoDeAdivinhacao/Program.cs
@@ -10,141 +10,54 @@ namespace JogoDeAdivinhacao
 
             while (true)
             {
-                Console.Clear();
-                Console.WriteLine("---------------------------------");
-                Console.WriteLine("Jogo de Adivinhação");
-                Console.WriteLine("---------------------------------");
+                string escolhaDeDificuldade = Menu();
 
-                //escolha de nível de dificuldade
-                Console.WriteLine("Escolha um nível de dificuldade:");
-                Console.WriteLine("---------------------------------");
-                Console.WriteLine("1 - Fácil (10 tentativas)");
-                Console.WriteLine("2 - Normal (5 tentativas)");
-                Console.WriteLine("3 - Difícil (3 tentativas)");
-                Console.WriteLine("---------------------------------");
-
-                Console.Write("Digite sua escolha: ");
-                string escolhaDeDificuldade = Console.ReadLine();
-
-                int totalDeTentativas = 0;
-
-                if (escolhaDeDificuldade == "1")
-                {
-                    totalDeTentativas = 10;
-                }
-                else if (escolhaDeDificuldade == "2")
-                {
-                    totalDeTentativas = 5;
-                }
-                else if (escolhaDeDificuldade == "3")
-                {
-                    totalDeTentativas = 3;
-                }
-                else
-                {
-                    Console.Write("\nOpção inválida! Pressione qualquer tecla para continuar...");
-                    Console.ReadLine();
-                    continue;
-                }
-
-                //escolha do número aleatório
-
-                Random geradorDeNumeros = new Random();
-                int numeroSecreto = geradorDeNumeros.Next(1,21);
                 List<int> numerosChutados = new List<int>();
                 int pontuacao = 1000;
 
+                int numeroAleatorio = NumeroAleatorio();
+
                 //lógica do jogo
-                for (int tentativa = 1; tentativa <= totalDeTentativas; tentativa++)
+                for (int tentativa = 1; tentativa <= QuantidadeDeTentativas(escolhaDeDificuldade); tentativa++)
                 {
                     int pontosPerdidos = 0;
 
-                    Console.Clear();
-                    Debug.WriteLine("Número secreto: " + numeroSecreto);
-                    Console.WriteLine("-----------------------------------");
-                    Console.WriteLine($"Tentativa {tentativa} de {totalDeTentativas}");
-                    Console.WriteLine("-----------------------------------");
-                    Console.WriteLine($"Pontuação atual: {pontuacao}");
-                    Console.WriteLine("-----------------------------------");
-                    Console.Write($"Números chutados: ");
+                    //Cabeçalho do jogo
+                    ExibirCabecalho(tentativa, escolhaDeDificuldade, pontuacao, numerosChutados, numeroAleatorio);
 
-                    foreach (int numeroChutado in numerosChutados)
-                    {
-                        Console.Write(numeroChutado + " ");
-                    }
-                    Console.WriteLine("\n-----------------------------------");
-                    Console.Write("\nChute um número: ");
-                    int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+                    //Chute de número
+                    int numeroDigitado = ChutarNumero();
 
-                    while (numerosChutados.Contains(numeroDigitado))
+                    while (NumeroChutadoAnteriormente(numerosChutados, numeroDigitado))
                     {
-                        Console.WriteLine("\nEsse número já foi chutado anteriormente.");
-                        Console.Write("\nChute outro número: ");
-                        numeroDigitado = Convert.ToInt32(Console.ReadLine());
+                        numeroDigitado = ChutarOutroNumero();
                     }
 
-                    if (numeroDigitado == numeroSecreto)
+                    if (JogadorAcertou(numeroDigitado, pontuacao, numeroAleatorio))
                     {
-                        Console.WriteLine("\n-----------------------------------");
-                        Console.WriteLine($"Parabéns, você acertou o número secreto e finalizou o jogo com {pontuacao} pontos!");
-                        Console.WriteLine("-----------------------------------");
+                        MensagemDeVitoria(pontuacao);
                         break;
                     }
-                    else if (numeroDigitado != numeroSecreto && tentativa == totalDeTentativas)
+                    else if (JogadorNaoAcertou(numeroDigitado, tentativa, escolhaDeDificuldade, pontuacao, numeroAleatorio))
                     {
-                        pontuacao = 0;
-                        Console.WriteLine("\n-----------------------------------");
-                        Console.WriteLine($"Game over! O número secreto era: {numeroSecreto}");
-                        Console.WriteLine($"Você finalizou o jogo com {pontuacao} pontos!");
-                        Console.WriteLine("-----------------------------------");
+                        MensagemDeDerrota(pontuacao, numeroAleatorio);
                         break;
                     }
-
-                    else if (numeroDigitado > numeroSecreto)
+                    else if (ChuteMaiorQueNumero(numeroDigitado, numeroAleatorio))
                     {
-                        Console.WriteLine("\n----------------------------------------------");
-                        Console.WriteLine("O número digitado é maior que o número secreto!");
-                        Console.WriteLine("----------------------------------------------");
-                        numerosChutados.Add(numeroDigitado);
-                        pontosPerdidos = (numeroDigitado - numeroSecreto) / 2;
-                        if (pontosPerdidos < 0)
-                        {
-                            pontosPerdidos *= -1;
-                        }
-                        if (pontosPerdidos < 1)
-                        {
-                            pontosPerdidos = 1;
-                        }
-
-                        pontuacao -= pontosPerdidos;
+                        MensagemChuteMaiorQueNumero();
+                        pontuacao -= PontuacaoAtualizada(numeroDigitado, numerosChutados, pontosPerdidos, numeroAleatorio);
                     }
-                    else if (numeroDigitado < numeroSecreto)
+                    else if (ChuteMenorQueNumero(numeroDigitado, numeroAleatorio))
                     {
-                        Console.WriteLine("\n----------------------------------------------");
-                        Console.WriteLine("O número digitado é menor que o número secreto!");
-                        Console.WriteLine("----------------------------------------------");
-                        numerosChutados.Add(numeroDigitado);
-                        pontosPerdidos = (numeroDigitado - numeroSecreto) / 2;
-                        if (pontosPerdidos < 0)
-                        {
-                            pontosPerdidos *= -1;
-                        }
-                        if (pontosPerdidos < 1)
-                        {
-                            pontosPerdidos = 1;
-                        }
-
-                        pontuacao -= pontosPerdidos;
+                        MensagemChuteMenorQueNumero();
+                        pontuacao -= PontuacaoAtualizada(numeroDigitado, numerosChutados, pontosPerdidos, numeroAleatorio);
                     }
+
                     Console.Write("\nPressione qualquer tecla para continuar...");
                     Console.ReadLine();
                 }
-
-                Console.Write("\nDeseja jogar novamente? S/N: ");
-
-                string continuar = Console.ReadLine().ToUpper();
-
-                if (continuar == "S")
+                if (JogarNovamente() == "S")
                 {
                     continue;
                 }
@@ -155,6 +68,187 @@ namespace JogoDeAdivinhacao
                     break;
                 }
             }
+        }
+
+        static string Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Jogo de Adivinhação");
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine("Escolha um nível de dificuldade:");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1 - Fácil (10 tentativas)");
+            Console.WriteLine("2 - Normal (5 tentativas)");
+            Console.WriteLine("3 - Difícil (3 tentativas)");
+            Console.WriteLine("---------------------------------");
+
+            Console.Write("Digite sua escolha: ");
+            return Console.ReadLine();
+        }
+
+        static int QuantidadeDeTentativas(string escolhaDeDificuldade)
+        {
+            int totalDeTentativas = 0;
+
+            if (escolhaDeDificuldade == "1")
+            {
+                totalDeTentativas = 10;
+            }
+            else if (escolhaDeDificuldade == "2")
+            {
+                totalDeTentativas = 5;
+            }
+            else if (escolhaDeDificuldade == "3")
+            {
+                totalDeTentativas = 3;
+            }
+            else
+            {
+                Console.Write("\nOpção inválida! Pressione qualquer tecla para continuar...");
+                Console.ReadLine();
+            }
+            return totalDeTentativas;
+        }
+
+        static int NumeroAleatorio()
+        {
+            Random geradorDeNumeros = new Random();
+            int numeroSecreto = geradorDeNumeros.Next(1, 21);
+
+            return numeroSecreto;
+        }
+
+        static string JogarNovamente()
+        {
+            Console.Write("\nDeseja jogar novamente? S/N: ");
+            return Console.ReadLine().ToUpper();
+        }
+
+        static void ExibirCabecalho(int tentativa, string escolhaDeDificuldade, int pontuacao, List<int> numerosChutados, int numeroAleatorio)
+        {
+            Console.Clear();
+            Debug.WriteLine("Número secreto: " + numeroAleatorio);
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"Tentativa {tentativa} de {QuantidadeDeTentativas(escolhaDeDificuldade)}");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"Pontuação atual: {pontuacao}");
+            Console.WriteLine("-----------------------------------");
+            Console.Write($"Números chutados: ");
+            foreach (int numeroChutado in numerosChutados)
+            {
+                Console.Write(numeroChutado + " ");
+            }
+            Console.WriteLine("\n-----------------------------------");
+        }
+
+        static bool JogadorAcertou(int numeroDigitado, int pontuacao, int numeroAleatorio)
+        {
+            if (numeroDigitado == numeroAleatorio)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void MensagemDeVitoria(int pontuacao)
+        {
+            Console.Clear();
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"Parabéns, você acertou o número secreto e finalizou o jogo com {pontuacao} pontos!");
+            Console.WriteLine("-----------------------------------");
+        }
+
+        static bool JogadorNaoAcertou(int numeroDigitado, int tentativa, string escolhaDeDificuldade, int pontuacao, int numeroAleatorio)
+        {
+            if (numeroDigitado != numeroAleatorio && tentativa == QuantidadeDeTentativas(escolhaDeDificuldade))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void MensagemDeDerrota(int pontuacao, int numeroAleatorio)
+        {
+            pontuacao = 0;
+            Console.Clear();
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"Game over! O número secreto era: {numeroAleatorio}");
+            Console.WriteLine($"Você finalizou o jogo com {pontuacao} pontos!");
+            Console.WriteLine("-----------------------------------");
+        }
+
+        static bool ChuteMaiorQueNumero(int numeroDigitado, int numeroAleatorio)
+        {
+            if (numeroDigitado > numeroAleatorio)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void MensagemChuteMaiorQueNumero()
+        {
+            Console.WriteLine("\n----------------------------------------------");
+            Console.WriteLine("O número digitado é maior que o número secreto!");
+            Console.WriteLine("----------------------------------------------");
+        }
+
+        static bool ChuteMenorQueNumero(int numeroDigitado, int numeroAleatorio)
+        {
+            if (numeroDigitado < numeroAleatorio)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void MensagemChuteMenorQueNumero()
+        {
+            Console.WriteLine("\n----------------------------------------------");
+            Console.WriteLine("O número digitado é menor que o número secreto!");
+            Console.WriteLine("----------------------------------------------");
+        }
+
+        static int PontuacaoAtualizada(int numeroDigitado, List<int> numerosChutados, int pontosPerdidos, int numeroAleatorio)
+        {
+            if (ChuteMaiorQueNumero(numeroDigitado, numeroAleatorio) || ChuteMenorQueNumero(numeroDigitado, numeroAleatorio))
+            {
+                numerosChutados.Add(numeroDigitado);
+                pontosPerdidos = (numeroDigitado - numeroAleatorio) / 2;
+                if (pontosPerdidos < 0)
+                {
+                    pontosPerdidos *= -1;
+                }
+                if (pontosPerdidos < 1)
+                {
+                    pontosPerdidos = 1;
+                }
+            }
+            return pontosPerdidos;
+        }
+
+        static int ChutarNumero()
+        {
+            Console.Write("\nChute um número: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static bool NumeroChutadoAnteriormente(List<int> numerosChutados, int numeroDigitado)
+        {
+            if (numerosChutados.Contains(numeroDigitado))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static int ChutarOutroNumero()
+        {
+            Console.WriteLine("\nEsse número já foi chutado anteriormente.");
+            Console.Write("\nChute outro número: ");
+            return Convert.ToInt32(Console.ReadLine());
         }
     }
 }
